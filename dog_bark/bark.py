9c82f0de500ed9fa9a8af6faf9c95660923f3868
Malcolm Mielle <malcolm.mielle@protonmail.com>
2023-03-13 21:30:36 +0100

    Add step to filter audio signal when too long

--- dog_bark/bark.py
+++ dog_bark/bark.py
@@ -5,12 +5,17 @@ import numpy as np
 
 
 class Bark:
-    def __init__(self, file_name: str) -> None:
+    def __init__(self, file_name: str, step: int = 100) -> None:
+        """Extract the signal from the audio file `file_name`. The audio filtered by
+        keeping data every `step`"""
         self.file_name = file_name
-        wav_obj = wave.open(f=self.file_name, mode="rb")
-        n_samples = wav_obj.getnframes()
-        signal_wave = wav_obj.readframes(n_samples)
+        self._wav_obj = wave.open(f=self.file_name, mode="rb")
+        n_samples = self._wav_obj.getnframes()
+        signal_wave = self._wav_obj.readframes(n_samples)
 
         self.signal_array = np.frombuffer(signal_wave, dtype=np.int16)
-        self.signal_array = self.signal_array[self.signal_array > 0]
-        self.signal_array = self.signal_array[::100]
+
+        self._step = 1
+        if step > 1:
+            self._step = step
+            self.signal_array = self.signal_array[:: self._step]
--- scripts/bark.py
+++ scripts/bark.py
@@ -9,13 +9,12 @@ def main():
         description="Process a file and plot to see if barks."
     )
     parser.add_argument(
-        "filename",
-        type=str,
-        help="the file to analyse",
+        "--filename", type=str, help="the file to analyse", default="sounds/13mars.wav"
     )
+    parser.add_argument("--step", type=int, help="step to sample audio", default=100)
     args = parser.parse_args()
 
-    bark = Bark(args.filename)
+    bark = Bark(args.filename, args.step)
     plot_bark(bark)
 
 
