306a3732a43bdec81f7ab3e3a88ad37278f5aa21
Malcolm Mielle <malcolm.mielle@protonmail.com>
2023-03-15 12:30:10 +0100

    Print bark info after recording

--- scripts/record.py
+++ scripts/record.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pathlib import Path
 
+from dog_bark.bark import Bark
 from dog_bark.recorder import Recorder
 
 
@@ -10,6 +11,8 @@ def main():
 
     with Recorder(filename=str(filepath), bark_level=1000) as rec:
         rec.record()
+    bark = Bark(str(filepath))
+    print(bark)
 
 
 if __name__ == "__main__":
