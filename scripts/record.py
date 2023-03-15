faefef59eb6c543bd1749a895df8c40715c3c573
Malcolm Mielle <malcolm.mielle@protonmail.com>
2023-03-15 12:38:29 +0100

    Save image and adjust threshold for microphone

--- scripts/record.py
+++ scripts/record.py
@@ -2,17 +2,21 @@ from datetime import datetime
 from pathlib import Path
 
 from dog_bark.bark import Bark
+from dog_bark.plot.plot_bark import plot_bark
 from dog_bark.recorder import Recorder
 
 
 def main():
+    threshold = 100
+
     now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
     filepath = Path("recordings", now + ".wav")
-
-    with Recorder(filename=str(filepath), bark_level=1000) as rec:
+    with Recorder(str(filepath), bark_level=threshold) as rec:
         rec.record()
-    bark = Bark(str(filepath))
+
+    bark = Bark(str(filepath), threshold=threshold)
     print(bark)
+    plot_bark(bark, to_file=True)
 
 
 if __name__ == "__main__":
