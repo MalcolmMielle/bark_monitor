59d38a9ed51f85ad7454e1b4c152a243fee23809
Malcolm Mielle <malcolm.mielle@ik.me>
2025-06-13 22:32:00 +0200

    Isolate server

---
+++ bark_monitor/cli/web_service/fastapi_server.py
@@ -0,0 +1,56 @@
+from typing import Any
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+
+from bark_monitor.recorders.base_recorder import BaseRecorder
+from bark_monitor.recorders.recording import Recording
+
+
+def fasdtapi_webver(lifespan, recorder: dict[str, BaseRecorder]) -> None:
+    app = FastAPI(lifespan=lifespan)
+
+    @app.post("/start")
+    async def start() -> dict[str, str]:
+        recorder["yamnet"].record()
+        return {"result": "Started the recording"}
+
+    @app.post("/stop")
+    async def stop() -> dict[str, str]:
+        recorder["yamnet"].stop()
+        return {"result": "Stopped the recording"}
+
+    @app.get("/status")
+    async def status() -> dict[str, Any]:
+        is_running = recorder["yamnet"].running
+        return {"result": is_running}
+
+    @app.get("/daily_activities")
+    async def daily_activities() -> dict[str, Any]:
+        recording = Recording.read(
+            output_folder=recorder["yamnet"].output_folder,
+            sync_service=recorder["yamnet"].sync,
+        )
+        activities = recording.daily_activities_formated()
+        return {"activities": activities}
+
+    @app.get("/activities")
+    async def activities() -> dict[str, Any]:
+        recording = Recording.read(
+            output_folder=recorder["yamnet"].output_folder,
+            sync_service=recorder["yamnet"].sync,
+        )
+        activities = recording.activities_formated()
+        return {"activities": activities}
+
+    @app.get("/last_audio")
+    async def last_audio() -> FileResponse:
+        audio_folder = recorder["yamnet"].today_audio_folder
+        audio_files = audio_folder.glob("*")
+        if len(list(audio_files)) == 0:
+            raise HTTPException(status_code=404, detail="No audio files found")
+        audio_file = max(audio_files, key=lambda p: p.stat().st_ctime)
+        return FileResponse(audio_file)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- bark_monitor/cli/web_service/main.py
+++ bark_monitor/cli/web_service/main.py
@@ -1,18 +1,16 @@
 from contextlib import asynccontextmanager
-from typing import Any
 
 import tyro
-import uvicorn
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import FastAPI
 
 from bark_monitor import logger
 from bark_monitor.cli.get_param import Parameters
+from bark_monitor.cli.web_service.fastapi_server import fasdtapi_webver
 from bark_monitor.next_cloud_sync import NextCloudSync
-from bark_monitor.recorders.recording import Recording
+from bark_monitor.recorders.base_recorder import BaseRecorder
 from bark_monitor.recorders.yamnet_recorder import YamnetRecorder
 
-recoder: dict[str, YamnetRecorder] = {}
+recorder: dict[str, BaseRecorder] = {}
 
 
 @asynccontextmanager
@@ -33,7 +31,7 @@ async def lifespan(app: FastAPI):
         passwd=parameters.nextcloud_parameters.passwd,
     )
 
-    recoder["yamnet"] = YamnetRecorder(
+    recorder["yamnet"] = YamnetRecorder(
         sync=sync_service,
         output_folder=parameters.output_folder,
         sampling_time_bark_seconds=parameters.sampling_time_bark_seconds,
@@ -45,42 +43,7 @@ async def lifespan(app: FastAPI):
 
 
 def main():
-    app = FastAPI(lifespan=lifespan)
-
-    @app.post("/start")
-    async def start() -> dict[str, str]:
-        recoder["yamnet"].record()
-        return {"result": "Started the recording"}
-
-    @app.post("/stop")
-    async def stop() -> dict[str, str]:
-        recoder["yamnet"].stop()
-        return {"result": "Stopped the recording"}
-
-    @app.get("/status")
-    async def status() -> dict[str, Any]:
-        is_running = recoder["yamnet"].running
-        return {"result": is_running}
-
-    @app.get("/activity")
-    async def activity() -> dict[str, Any]:
-        recording = Recording.read(
-            output_folder=recoder["yamnet"].output_folder,
-            sync_service=recoder["yamnet"].sync,
-        )
-        activities = recording.daily_activities_formated()
-        return {"activities": activities}
-
-    @app.get("/last_audio")
-    async def last_audio() -> FileResponse:
-        audio_folder = recoder["yamnet"].today_audio_folder
-        audio_files = audio_folder.glob("*")
-        if len(list(audio_files)) == 0:
-            raise HTTPException(status_code=404, detail="No audio files found")
-        audio_file = max(audio_files, key=lambda p: p.stat().st_ctime)
-        return FileResponse(audio_file)
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    fasdtapi_webver(lifespan=lifespan, recorder=recorder)
 
 
 if __name__ == "__main__":
